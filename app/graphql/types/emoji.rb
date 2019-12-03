b3bd3649ce3bbddb0e269f56a1afdc87ff8f1b4e
David Buchan-Swanson <david.buchanswanson@ferocia.com.au>
2019-12-03 18:49:23 +1100

    add custom emoji images

--- app/graphql/types/emoji.rb
+++ app/graphql/types/emoji.rb
@@ -3,4 +3,9 @@
 class Types::Emoji < Types::Base::Object
   field :name, String, null: false
   field :raw, String, null: true
+  field :url, String, null: true
+
+  def url
+    object.image_filename
+  end
 end
--- config/initializers/custom_emoji.rb
+++ config/initializers/custom_emoji.rb
@@ -1,6 +1,16 @@
+# frozen_string_literal: true
+
 require 'gemoji'
 
 # Custom Emoji :roll_eyes:
-Emoji.create('9ball')
-Emoji.create('smash')
-Emoji.create('cart')
+Emoji.create('9ball') do |c|
+  c.image_filename = 'https://emoji.slack-edge.com/T024P5CCV/9ball/c187be4ee5ea0acb.png'
+end
+
+Emoji.create('smash') do |c|
+  c.image_filename = 'https://emoji.slack-edge.com/T024P5CCV/smash/2e9ddf3f01b420ce.png'
+end
+
+Emoji.create('kart') do |c|
+  c.image_filename = 'https://emoji.slack-edge.com/T024P5CCV/kart/0ae70064c246aef8.jpg'
+end
