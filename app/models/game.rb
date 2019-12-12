868007d1a6224f1b1c305383893dc7d9635fd444
David Buchan-Swanson <david.buchanswanson@ferocia.com.au>
2019-12-13 08:17:33 +1100

    helper methods for rating events

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,7 @@
 
 class Game < ApplicationRecord
   has_many :matches
+  has_many :rating_events, through: :matches
   has_many :results, through: :matches
   has_many :ratings
 
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -8,4 +8,21 @@ class Rating < ApplicationRecord
   def public_mean
     (mean * 100).floor
   end
+
+  def reset_to_last_event!
+    mean = nil
+    deviation = nil
+
+    event = rating_events.order(updated_at: :desc).first
+
+    if event.present?
+      mean = event.mean
+      deviation = event.deviation
+    else
+      mean = game.default_mean
+      deviation = game.default_deviation
+    end
+
+    update!(mean: mean, deviation: deviation)
+  end
 end
