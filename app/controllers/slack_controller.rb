7a3c55029e45b8f5e16740b33a51427215b99826
David Buchan-Swanson <david.buchanswanson@ferocia.com.au>
2023-03-02 22:53:33 +1100

    add new required channels for fifa + towerfall

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SlackController < ApplicationController
-  ALLOWED_CHANNELS = %w[billiards gaming-smash-bros gaming-rocket-league].freeze
+  ALLOWED_CHANNELS = %w[billiards gaming-smash-bros gaming-rocket-league towerfall fifa].freeze
 
   def webhook
     body = parsed_body
